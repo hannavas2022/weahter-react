8b48859cd408078c2c3df21da4fcb279d2cb2164
hannavas2022 <112612098+hannavas2022@users.noreply.github.com>
2023-05-03 14:17:21 -0500

    changed IconURL

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       visibility: response.data.visibility / 1000,
       description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}`,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
